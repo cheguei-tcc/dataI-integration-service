25bc8d41c0b986b0c45f149ca9edeb2242194c15
Higor Henrique Reculiano <higor.reculiano@hotmail.com>
2022-04-18 20:37:45 -0300

    Subida do projeto

---
+++ Controllers/StudentsDataController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace data_integrator_service.Controllers
+{
+    public class StudentsDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
